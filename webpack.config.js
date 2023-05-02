6b00a19c64cdff3d139b7f0b31f6a626426e83c1
Alijah Trombley <59921609+AlijahT@users.noreply.github.com>
2023-05-02 19:58:24 -0400

    fixed issue

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
     entry: {
         app: './client/maker.jsx',
         login: './client/login.jsx',
-        notFound: './client/notFound.jsx'
     },
     module: {
         rules: [
